68720446c9ca4ffcd05e4d4700be803297c1140b
Vadim Evstegneev <ev.vadim@gmail.com>
2025-11-10 20:58:47 +0300

    `addIngredientTest()` improvements

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -11,16 +11,26 @@ public class BurgerTest {
 
     Bun bun;
     Burger burger;
+    Burger testingBurger;
     int countOfIngredients = 8;
     int removedIngredientIndex = 5;
 
     @Before
     public void setUp() throws Exception {
         burger = new Burger();
+        testingBurger = new Burger();
         bun = Mockito.mock(Bun.class);
         burger.setBuns(bun);
         for (int i = 0; i < countOfIngredients; i++) {
-            burger.addIngredient(Mockito.mock(Ingredient.class));
+
+            Ingredient ingredient = Mockito.mock(Ingredient.class);
+
+            // наполняем объект тестируемого класса методами класса
+            burger.addIngredient(ingredient);
+
+            // наполняем эталонный объект вручную
+            testingBurger.ingredients.add(ingredient);
+
         }
     }
 
@@ -31,10 +41,21 @@ public class BurgerTest {
 
     @Test
     public void addIngredientTest() {
+
         Ingredient ingredient = Mockito.mock(Ingredient.class);
+
+        // добавляем ингредиент в объект методом класса
         burger.addIngredient(ingredient);
-        MatcherAssert.assertThat("Object is not added", burger.ingredients, hasItem(ingredient));
-        MatcherAssert.assertThat("Object is not last item", burger.ingredients.get(burger.ingredients.size() - 1), equalTo(ingredient));
+
+        // добавляем ингредиент в эталонный объект вручную
+        testingBurger.ingredients.add(ingredient);
+
+        // проверяем, что объекты состоят их одинаковых ингредиентов
+        MatcherAssert.assertThat("List of Ingredients are not equal", burger.ingredients, equalTo(testingBurger.ingredients));
+
+        // проверяем, что добавленный элемент в конце списка
+        MatcherAssert.assertThat("Last item of List of Ingredients are not the same as added", burger.ingredients.get(burger.ingredients.size() - 1), equalTo(ingredient));
+
     }
 
     @Test
